5fbf050b2b746cee8b9d37950395116a6dc55b28
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2026-02-25 15:25:41 +0600

    Add react-icons dependency and implement dialog component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import DashboardSidebar from "@/components/CommonComponents/DashboardSidebar";
+import NavBar from "@/components/CommonComponents/NabBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +30,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <SidebarProvider>
+          <DashboardSidebar />
+          <SidebarInset className="overflow-x-hidden">
+            <div className={` bg-[#f3f3f3] min-h-screen `}>
+              <NavBar />
+              {children}
+            </div>
+          </SidebarInset>
+        </SidebarProvider>
       </body>
     </html>
   );
